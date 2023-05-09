7019170b238f77924aae030a6335b40b6c9ecfcc
pollob12 <85349658+pollob12@users.noreply.github.com>
2023-05-09 09:21:22 +0600

    Add files via upload

---
+++ class 01/app.js
@@ -0,0 +1,24 @@
+console.log(
+  "amr name " +
+    prompt("type your full name") +
+    "and i love to play " +
+    prompt("what is your favourite sports") +
+    " and i love to eat" +
+    prompt("Type your favpurite food")
+);
+
+console.log(
+  "Amer name " +
+    prompt("types your full name") +
+    "and i also play " +
+    prompt("what is your favourite game") +
+    "and my hobby is" +
+    prompt("type your hobby")
+);
+
+console.log(
+  "amr name " +
+    prompt("what is your name") +
+    "and i also love to paly " +
+    prompt("what is your favourite sports")
+);
